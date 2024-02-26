932b53c040bbe6ae4d51d5b2f62bd6ce0fefcb7a
Diamond427 <allenbull1234@gmail.com>
2024-02-26 00:04:49 -0600

    updated readme.md

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from convert import convert_files
 
 if __name__ == "__main__":
   convert_files(
-    src_dir="C:/Users/Admin/Pictures/sample",
-    dst_dir="c:/users/admin/pictures/normalized",
+    src_dir=r"C:\Users\Admin\Pictures\sample",
+    dst_dir=r"C:\Users\Admin\Pictures\normalized",
     image_format="png",
     out_fname_format="thermal_%d_%u",
     delete_source=False
--- readme.md
+++ readme.md
@@ -13,8 +13,8 @@
 
 ```python
 convert_files(
-  src_dir="C:/Users/Admin/Pictures/sample",
-  dst_dir="c:/users/admin/pictures/normalized",
+  src_dir=r"C:\Users\Admin\Pictures\sample",
+  dst_dir=r"C:\Users\Admin\Pictures\normalized",
   image_format="png",
   out_fname_format="thermal_%d_%u",
   delete_source=False
